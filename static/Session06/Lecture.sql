b79606cede721ea1786397dd9a84000229b2fcf9
Omid Reza Heidari <omid.orh@gmail.com>
2024-03-16 23:10:16 -0400

    Update the news and add files | ignore .key

--- static/Session06/Lecture.sql
+++ static/Session06/Lecture.sql
@@ -12,7 +12,8 @@ create table customers(
     email varchar(50),
     password varchar(50),
     country varchar(50),
-    balance int
+    balance int,
+    last_huge_order_data datetime
 );
 
 insert into customers(first_name, last_name, email, password, country, balance) values
@@ -43,141 +44,71 @@ insert into orders(customer_id, amount, created_at, ship_to, units)
                     (1, 100, '2002-02-02 09:00:00.000000', 'Denmark', 13);
 
 
-# Create simple evaluation on orders' amount
-select orders.id, orders.amount,
-       case
-           when orders.amount > 1000 then 'Giant'
-           when orders.amount > 300 then 'medium'
-           else 'small'
-       end as 'Evaluation Result'
-from orders;
-
-# Check ship to be Canada or if is null check customers.country
-select orders.id, orders.ship_to, customers.id, customers.country
-from orders inner join customers on orders.customer_id = customers.id
-where 'Canada'=(
-    case
-        when orders.ship_to is null then customers.country
-        else orders.ship_to
-    end
-);
-
-# Alternative way with IF
-select orders.id, orders.ship_to, customers.id, customers.country
-from orders inner join customers on orders.customer_id = customers.id
-where 'Canada'=(
-    IF(orders.ship_to is null, customers.country, orders.ship_to)
-);
-
-
-# get orders with data even if they are null, assume Canada
-select ifnull(orders.ship_to, 'Canada') as 'ship to'
-from orders;
-
-#alternative way
-select coalesce(orders.ship_to, 'Canada') as 'ship to'
-from orders;
-
-# get each amount for each order
-select orders.amount, ifnull(orders.units, 1) as 'units', orders.amount/ifnull(orders.units, 1) as 'each amount'
-from orders;
-
-#alternative way
-select orders.amount, coalesce(orders.units, 1) as 'units', orders.amount/coalesce(orders.units, 1) as 'each amount'
-from orders;
-
-# check the table before adding the new column
-select * from customers;
-
-# Add City column to customers
-alter table customers
-add City varchar(255) default 'Montréal';
-
-# check the table after adding the new column
-select * from customers;
-
-#rename the City column
-alter table customers
-rename column City to CT;
-
-# check the table after renaming the column
-select * from customers;
-
-# Remove CT column
-alter table customers
-drop column CT;
-
-# check the table after eliminating the column
-select * from customers;
-
-# Index on lastname
-create index idx_lastname
-on customers(last_name);
-
-# Index on combination of last name and first name
-create index idx_customer_name
-on customers(last_name, first_name);
-
-# Drop the indexes
-alter table customers
-drop index idx_lastname;
-
-alter table customers
-drop index idx_customer_name;
-
-drop table if exists customers, orders;
-
-# create table with some conditions
-create table customers(
-    id int primary key auto_increment,
-    first_name varchar(50),
-    last_name varchar(50),
-    email varchar(50),
-    password varchar(50),
-    country varchar(50),
-    balance int,
-    age int,
-    check (age > 18)
-);
-
-insert into customers(first_name, last_name, email, password, country, balance, age) values ('Omid Reza', 'Heidari', 'omid.orh@gmail.com', 'password1', 'Iran', 2000, 12);
-
-drop table if exists customers;
-# Alternative way
-create table customers(
-    id int primary key auto_increment,
-    first_name varchar(50),
-    last_name varchar(50),
-    email varchar(50),
-    password varchar(50),
-    country varchar(50),
-    balance int,
-    age int,
-    constraint agecheck check ( age >18 )
-);
-
-insert into customers(first_name, last_name, email, password, country, balance, age) values ('Omid Reza', 'Heidari', 'omid.orh@gmail.com', 'password1', 'Iran', 2000, 12);
-
-# drop check constraint
-alter table customers
-drop constraint agecheck;
-
-insert into customers(first_name, last_name, email, password, country, balance, age) values ('Omid Reza', 'Heidari', 'omid.orh@gmail.com', 'password1', 'Iran', 2000, 12);
-
-# create a view from orders higher price than the average amount
-create view OrdersAboveAverageAmount as
-select orders.id, orders.amount
-from orders
-where orders.amount > (select avg(orders.amount) from orders);
+DROP PROCEDURE IF EXISTS create_order_transaction;
 
 select *
-from OrdersAboveAverageAmount;
+from customers;
 
-# update the view
-create or replace view OrdersAboveAverageAmount as
-select orders.id, orders.amount, orders.ship_to
-from orders
-where orders.amount > (select avg(orders.amount) from orders);
+# Update balance of customer after creating of the order
+CREATE PROCEDURE create_order_transaction()
+BEGIN
+SET @customerId = 2;
+SET @orderCost = 2500;
+SELECT balance INTO @customerBalance FROM customers WHERE id = @customerId;
 
-select *
-from OrdersAboveAverageAmount;
+IF @customerBalance > @orderCost THEN
+    UPDATE customers
+    SET balance = balance - @orderCost
+    WHERE id = @customerId;
+
+    INSERT INTO orders (customer_id, amount, created_at, ship_to, units) VALUES (@customerId, @orderCost, '2022-02-02 10:00:00.000000', 'Iran', 1);
+    COMMIT;
+ELSE
+    ROLLBACK;
+END IF;
+END;
+
+CALL create_order_transaction();
+
+select count(*)
+from orders;
+
+SELECT *
+FROM customers;
+
+# update last huge order data (if it is not huge roll back to the checkpoint)
+DROP PROCEDURE IF EXISTS create_order_with_update_large_order_transaction;
+
+CREATE PROCEDURE create_order_with_update_large_order_transaction()
+BEGIN
+start transaction;
+SET @customerId = 3;
+SET @orderCost = 1500;
+SELECT balance INTO @customerBalance FROM customers WHERE id = @customerId;
+
+IF @customerBalance > @orderCost THEN
+    UPDATE customers
+    SET balance = balance - @orderCost
+    WHERE id = @customerId;
+
+    INSERT INTO orders (customer_id, amount, created_at, ship_to, units) VALUES (@customerId, @orderCost, '2022-02-02 10:00:00.000000', 'Iran', 1);
+    SAVEPOINT before_data_update;
+
+
+    IF @orderCost > 1000 THEN
+        UPDATE customers
+        SET last_huge_order_data = '2022-02-02 10:00:00.000000'
+        where id=@customerId;
+    ELSE
+        ROLLBACK TO SAVEPOINT before_data_update;
+    END IF;
+    COMMIT;
+ELSE
+    ROLLBACK;
+END IF;
+END;
+
+CALL create_order_with_update_large_order_transaction();
+
+SELECT *
+FROM customers;
